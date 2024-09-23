0b4c55fa6470128b7a43ac9e5f762504e5a110f8
Yanxiang Yu <114280238+Yanxiangyu-med@users.noreply.github.com>
2024-09-23 20:30:58 +0800

    Update and rename Figure8 to Figure8.R

--- code/Figure8.R
+++ code/Figure8.R
@@ -17,11 +17,11 @@ row.names(annotation_r) <- row.names(plot_df)
 ann_colors <- plot_df[,5]
 row.names(ann_colors) <- plot_df[,1]
 
-g <- pheatmap(plot_df[,c(2,3)],color = colorRampPalette(c("#5A4C88", "white", "#CB422F"))(50), 
+p8 <- pheatmap(plot_df[,c(2,3)],color = colorRampPalette(c("#5A4C88", "white", "#CB422F"))(50), 
               cluster_cols = FALSE, border_color = NA,
               cluster_rows = TRUE, clustering_method = "complete", scale = 'none',
               annotation_row  = annotation_r,   treeheight_row = 120,annotation_colors = ann_colors)
 
-g
+p8
 
 # ##############################################################################
